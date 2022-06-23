2fdd632d0feed22a37cba469865c1c73224e4f6f
Karel van Oordt <vanoordtkarel@gmail.com>
2022-06-23 14:28:29 -0500

    Personalizar vistas a Español

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validates :name, :password, :password_confirmation, :email, :rcf, presence: true
+  # validates :rfc, length: { minimum: 12, maximum: 13 }
+  # validates :rfc, format: { with: /^([a-zA-Z&Ñ]{3,4}\d{6}[a-zA-z0-9]{3})/, message: "Formato de RFC no válido" }
 end
